5a93525b5a0cada4efe15ee892621fc056d9598b
Nitish Sharma <n.sharma8427@gmail.com>
2021-05-08 17:32:58 +0530

    Updates

---
+++ precache-manifest.8315a14935340e61a450608d067cb95d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c492703a5ba91e3a71fb434ce38d06",
+    "url": "/CovidTracker/index.html"
+  },
+  {
+    "revision": "93168290c658b2ba3072",
+    "url": "/CovidTracker/static/css/main.154ce23b.chunk.css"
+  },
+  {
+    "revision": "28e6a9af4ec14e82b5a8",
+    "url": "/CovidTracker/static/js/2.3d75a31e.chunk.js"
+  },
+  {
+    "revision": "f5de7118f11c3e4b14b47a321484f58a",
+    "url": "/CovidTracker/static/js/2.3d75a31e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93168290c658b2ba3072",
+    "url": "/CovidTracker/static/js/main.326ddd82.chunk.js"
+  },
+  {
+    "revision": "934a8b2735a616dc6106",
+    "url": "/CovidTracker/static/js/runtime-main.2fe7b862.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CovidTracker/static/media/image.d7265326.png"
+  }
+]);
